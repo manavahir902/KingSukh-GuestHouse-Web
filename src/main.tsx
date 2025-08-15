902bb27d7857f958949bd1413d970584dc1746ab
Manav <manavahir902@gmail.com>
2025-08-15 19:20:57 +0530

    Merge remote main into local project

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
 );
