14fb8328d8012d51b2720bd85bb3cb140f4b8607
Manav <manavahir902@gmail.com>
2025-08-15 18:56:15 +0530

    Merge remote main into local project

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/KingSukh-GuestHouse-Web/',
   server: {
     host: "::",
     port: 8080,
   },
-  base: '/KingSukh-GuestHouse-Web/', // important for GitHub Pages
   plugins: [
     react(),
-  ],
+    mode === 'development' && null,
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
