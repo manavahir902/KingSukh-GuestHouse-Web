7acf8333c6cb4e6ab3b3decdf5455dbfdaeb876e
Manav <manavahir902@gmail.com>
2025-08-15 19:01:26 +0530

    Merge remote main into local project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.render(
+  <BrowserRouter basename="/KingSukh-GuestHouse-Web">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
